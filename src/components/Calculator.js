ac2ad6dedb15697d2c83a658986debf5c3fcba6e
Yilikal250 <teshomeyilikal250@gmail.com>
2022-06-08 16:38:47 +0300

    added states and operations

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import calculate from '../logic/calculate';
 
 const keys = [
   {
@@ -78,36 +79,45 @@ const keys = [
     class: 'btn orange',
   },
 ];
+const Calculator = () => {
+  const [prev, setState] = useState({ total: null, next: null, operation: null });
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  const clickHandler = (e) => {
+    const display = () => calculate(prev, e.target.innerText);
+    setState(display);
+  };
+  let result = 0;
+  const { total, next, operation } = prev;
+  if (next === null && operation === null && total === null) {
+    result = 0;
   }
 
-  Calculator = () => (
-    <div className="container">
-      <from>
-        <input type="text" placeholder="0" />
-      </from>
-      <div className="keypad">
-        {keys.map((btn) => (<button type="button" key={btn.name} className={btn.class}>{btn.name}</button>))}
+  if (next !== null && total === null) {
+    result = next;
+  }
+
+  if (next === null && operation !== null && total !== null) {
+    result = total + operation;
+  }
+
+  if (next !== null && operation !== null && total !== null) {
+    result = total + operation + next;
+  }
+
+  if (next === null && operation === null && total !== null) {
+    result = total;
+  }
+  return (
+    <div className="window">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="container">
+        <div className="result">{result}</div>
+        <div className="keypad">
+          {keys.map((btn) => (<button type="button" value={btn.name} key={btn.name} onClick={clickHandler} className={btn.class}>{btn.name}</button>))}
+        </div>
       </div>
     </div>
   );
-
-  render() {
-    return (
-      <div className="container">
-        <from>
-          <input type="text" placeholder="0" />
-        </from>
-        <div className="keypad">
-          {keys.map((btn) => (<button type="button" key={btn.name} className={btn.class}>{btn.name}</button>))}
-        </div>
-      </div>
-    );
-  }
-}
+};
 
 export default Calculator;
