d70f40f6bd1544a7a9c90b3d04f6a228d67bd582
Yilikal250 <teshomeyilikal250@gmail.com>
2022-06-10 10:43:34 +0300

    hooks added

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-import React, { Component } from 'react';
 
 const keys = [
   {
@@ -83,18 +82,6 @@ const keys = [
 const Calculator = () => {
   const [prev, setState] = useState({ total: null, next: null, operation: null });
 
-class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
-
-  clickHandler = (e) => {
-    this.setState((prev) => calculate(prev, e.target.innerText));
   const clickHandler = (e) => {
     const display = () => calculate(prev, e.target.innerText);
     setState(display);
@@ -105,55 +92,29 @@ class Calculator extends Component {
     result = 0;
   }
 
-  render() {
-    const { total, next, operation } = this.state;
-    let result;
-    if (next === null && operation === null && total === null) {
-      result = 0;
-    }
-
-    if (next !== null && operation === null && total === null) {
-      result = next;
-    }
-
-    if (next === null && operation !== null && total !== null) {
-      result = total + operation;
-    }
   if (next !== null && total === null) {
     result = next;
   }
 
-    if (next !== null && operation !== null && total !== null) {
-      result = total + operation + next;
-    }
   if (next === null && operation !== null && total !== null) {
     result = total + operation;
   }
 
-    if (next === null && operation === null && total !== null) {
-      result = total;
-    }
   if (next !== null && operation !== null && total !== null) {
     result = total + operation + next;
   }
 
-    return (
-      <div className="container">
-        <div className="result">{result}</div>
-        <div className="keypad">
-          {keys.map((btn) => (<button type="button" value={btn.name} key={btn.name} onClick={this.clickHandler} className={btn.class}>{btn.name}</button>))}
-        </div>
-      </div>
-    );
   if (next === null && operation === null && total !== null) {
     result = total;
   }
-}
   return (
-    <div className="container">
-      <div className="result">{result}</div>
-      <div className="keypad">
-        {keys.map((btn) => (<button type="button" value={btn.name} key={btn.name} onClick={clickHandler} className={btn.class}>{btn.name}</button>))}
+    <div className="window">
+      <h2>Let&apos;s do some math!</h2>
+      <div className="container">
+        <div className="result">{result}</div>
+        <div className="keypad">
+          {keys.map((btn) => (<button type="button" value={btn.name} key={btn.name} onClick={clickHandler} className={btn.class}>{btn.name}</button>))}
+        </div>
       </div>
     </div>
   );
